58985c9b3b951b4201d600c3856b20bec7b44075
Agustina Casarone <agus.casarone@gmail.com>
2024-07-18 20:10:55 -0300

    education list

---
+++ portfolio/public/assets/information/educationList.ts
@@ -0,0 +1,76 @@
+import { Education } from "../../../src/interfaces/education";
+
+export const educationList: Education[] = [
+  {
+    title: "Licenciatura en Iformática",
+    institution: "UNLP",
+    date: "2021 - current",
+    place: "La Plata, Buenos Aires, Argentina",
+    description: "The profile of a graduate with a Bachelor's degree in Informatics is that of a professional " +
+      "with a strong theoretical foundation in Computer Science and a focus on the application of information technology " +
+      "in areas such as: networks, distributed systems, processor architecture, and operating systems, as well as advanced " +
+      "studies in algorithms and their applications. This education also enables participation in Research, Development, " +
+      "and Innovation activities within the discipline.",
+  },
+  {
+    title: "ITR - Angular Avanzado",
+    institution: "Mind Hub",
+    date: "2024",
+    description: "In-company course focused on the updates of Angular v17 with the use of Tailwind. " +
+      "Additionally, the creation of a NodeJS backend and a Mongo data base.",
+    place: "Online - Argentina"
+  },
+  {
+    title: "DAEVA - SQL INTERMEDIO UTN",
+    institution: "UTN",
+    date: "2023",
+    description: "This course corresponds to an Intermediate Level of Managing Information Persistence through SQL SERVER. " +
+      "Topics covered include complex SQL queries, data structures, triggers, TSQL syntax, and scripts",
+    place: "Online - Argentina"
+  },
+  {
+    title: "Certified Tech Developer",
+    link: "",
+    institution: "Digital House",
+    date: "2021 - 2023",
+    place: "Online - Argentina",
+    description: "Completed a comprehensive 2-year degree covering Web Design, Learning Agility, Agile Methodologies & Scrum, " +
+      "JavaScript programming, UX/UI Design, and Software Quality Testing. Developed skills in MySQL databases, React.js, " +
+      "Java OOP and Spring Boot, AWS infrastructure, Cloud Computing, and effective communication and collaboration within work teams. " +
+      "Successfully managed professional challenges demonstrating the practical application of acquired knowledge."
+  },
+  {
+    title: "100 Days of Code: The Complete Python Pro Bootcamp ",
+    institution: "Udemy",
+    date: "2023",
+    place: "Online",
+    description: "A comprehensive 60+ hour course covering Python 3, web development, data science, automation, and real-world projects." +
+      "Gained expertise in tools like PyCharm, Jupyter Notebook, and Flask, and built practical applications such as web scrapers and desktop GUIs."
+  },
+  {
+    title: "Responsive Web Design Certification",
+    link: "",
+    institution: "Free Code Camp",
+    date: "2021",
+    place: "Online",
+    description: "Completed a Responsive Web Design Certification focusing on HTML for content and CSS for design. " +
+      "Developed skills in building basic web applications like a cat photo app, advanced techniques such as CSS variables for dynamic styling, " +
+      "and implementing best practices for accessibility through projects like a quiz site. " +
+      "Proficient in creating responsive webpages using Flexbox for photo galleries and CSS Grid for " +
+      "magazine article layouts."
+  },
+  {
+    title: "First Certificate in English",
+    link: "",
+    institution: "Cambridge University",
+    date: "2013",
+    place: "La Plata, Buenos Aires, Argentina"
+  },
+  {
+    title: "High School of Natural Sciences",
+    link: "",
+    institution: "Normal 1, Mary O. Graham",
+    date: "2007 - 2013",
+    place: "La Plata, Buenos Aires, Argentina"
+  }
+]
