f4fc1c6607e50e5e8c82643bde644769f3b89edf
Agustina Casarone <agus.casarone@gmail.com>
2024-07-21 03:59:20 -0300

    fix: education link + place

--- portfolio/src/app/views/experiences/experiences.component.ts
+++ portfolio/src/app/views/experiences/experiences.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { ProfessionalTitleComponent } from "../../utils/professional-title/professional-title.component";
+import { experienceList } from '../../../../public/assets/information/experienceList';
+import { Experience } from '../../../interfaces/experience';
+import { ProfessionalBlockComponent } from "../../utils/professional-block/professional-block.component";
 
 @Component({
   selector: 'experiences',
   standalone: true,
-  imports: [ProfessionalTitleComponent],
+  imports: [ProfessionalTitleComponent, ProfessionalBlockComponent],
   templateUrl: './experiences.component.html',
   styleUrl: './experiences.component.scss'
 })
 export class ExperiencesComponent {
+
+  experienceList: Experience[] = experienceList;
 
 }
