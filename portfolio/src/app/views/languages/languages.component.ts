ad6cc0eb627a14bab6082d642616facd02a52d6d
Agustina Casarone <agus.casarone@gmail.com>
2024-07-26 18:37:24 -0300

    languages

--- portfolio/src/app/views/languages/languages.component.ts
+++ portfolio/src/app/views/languages/languages.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { languagesList } from '../../../../public/assets/information/languagesList';
+import { Language } from '../../../interfaces/language';
 
 @Component({
   selector: 'languages',
@@ -8,5 +10,25 @@ import { Component } from '@angular/core';
   styleUrl: './languages.component.scss'
 })
 export class LanguagesComponent {
+
+  languagesList: Language[] = this.sortLanguageList(languagesList);
+
+  parseLanguageListToStringList(languageList: Language[]): string[] {
+    return languageList.map(language => language.language);
+  }
+
+  sortLanguageList(languageList: Language[]): Language[] {
+    return languageList.sort((a, b) => {
+      if (a.order !== undefined && b.order !== undefined) {
+        return a.order - b.order;
+      } else if (a.order !== undefined) {
+        return -1;
+      } else if (b.order !== undefined) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  }
 
 }
