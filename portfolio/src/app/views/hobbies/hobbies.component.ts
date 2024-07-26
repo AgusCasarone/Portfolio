31eda3e9b0ed716bfddc7004160e24edf995549e
Agustina Casarone <agus.casarone@gmail.com>
2024-07-26 19:17:43 -0300

    hobbies

--- portfolio/src/app/views/hobbies/hobbies.component.ts
+++ portfolio/src/app/views/hobbies/hobbies.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { hobbyList } from '../../../../public/assets/information/hobbyList';
+import { Hobby } from '../../../interfaces/hobby';
 
 @Component({
   selector: 'hobbies',
@@ -8,5 +10,21 @@ import { Component } from '@angular/core';
   styleUrl: './hobbies.component.scss'
 })
 export class HobbiesComponent {
+
+  hobbyList: Hobby[] = this.sortHobbyList(hobbyList);
+
+  sortHobbyList(hobbyList: Hobby[]): Hobby[] {
+    return hobbyList.sort((a, b) => {
+      if (a.order !== undefined && b.order !== undefined) {
+        return a.order - b.order;
+      } else if (a.order !== undefined) {
+        return -1;
+      } else if (b.order !== undefined) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  }
 
 }
