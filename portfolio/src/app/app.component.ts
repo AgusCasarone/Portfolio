289195c0abe33a0587f9492abc788c5210b4c476
Agustina Casarone <agus.casarone@gmail.com>
2024-10-18 16:35:11 -0300

    ngx-translate implementation

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TranslateModule, TranslateLoader, TranslateService} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    TranslateModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- portfolio/src/app/utils/language/language.component.ts
+++ portfolio/src/app/utils/language/language.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'profile-language',
   standalone: true,
-  imports: [],
+  imports: [
+    TranslateModule
+  ],
   templateUrl: './language.component.html',
   styleUrl: './language.component.scss'
 })
 export class LanguageComponent {
+
+  isTranslating: boolean = false;
+
+
+  translate: TranslateService = inject(TranslateService);
+
+  title = 'portfolio';
+
+  changeLanguage(lang: string) {
+    this.translate.use(lang);
+  }
 
 }
