f4233c13ecddce69e234a83740631f9c478242ea
Agustina Casarone <agus.casarone@gmail.com>
2024-10-21 17:54:21 -0300

    download function

--- portfolio/src/app/utils/download/download.component.ts
+++ portfolio/src/app/utils/download/download.component.ts
@@ -1,31 +1,19 @@
 import { Component } from '@angular/core';
-import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { personalInfoDetail } from '../../../../public/assets/information/personalInfoDetail';
 import { PersonalInfo } from '../../../interfaces/personal-info';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'profile-download',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './download.component.html',
   styleUrl: './download.component.scss'
 })
 export class DownloadComponent {
 
+  isDownloading: boolean = false;
+
   personalInfoDetail: PersonalInfo = personalInfoDetail;
-
-  fileName: string = personalInfoDetail.lastName + "-" + personalInfoDetail.firstName;
-
-  image: string;
-
-  constructor(private sanitizer: DomSanitizer) {
-    this.image = 'https://example.com/your-image.jpg';
-  }
-
-  onDownload(): SafeUrl {
-    const dataUrl = 'data:CV-AgustinaCasarone-English-SEP2024.pdf;base64,';
-    console.log("deberia descargar")
-    return this.sanitizer.bypassSecurityTrustUrl(dataUrl + this.image);
-  }
 
 }
