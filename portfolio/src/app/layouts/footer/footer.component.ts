55a5fe4b6a64f225b2402e02f96696f6a5c2d3ff
Agustina Casarone <agus.casarone@gmail.com>
2024-10-21 18:30:35 -0300

    footer styling

--- portfolio/src/app/layouts/footer/footer.component.ts
+++ portfolio/src/app/layouts/footer/footer.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { personalInfoDetail } from '../../../../public/assets/information/personalInfoDetail';
+import { PersonalInfo } from '../../../interfaces/personal-info';
 
 @Component({
   selector: 'portfolio-footer',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  personalInfoDetail: PersonalInfo = personalInfoDetail;
 }
